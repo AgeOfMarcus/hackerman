9c4c3a2736c6dad957b4b11cd8f688d2aeeef218
Marcus Weinberger <marcus@localhost.localdomain>
2018-11-13 19:58:12 +0000

    working, incomplete

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='hackerman',
-	version='0.1',
+	version='0.2',
 	description='A python library for penetration testing, security, development, and fun.',
 	url='https://github.com/AgeOfMarcus/hackerman',
 	author='Marcus Weinberger',
