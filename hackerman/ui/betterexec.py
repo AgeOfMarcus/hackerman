846f47450fd881fe410d3912a117d80d073e95d0
Marcus Weinberger <marcus@marcusweinberger.com>
2019-01-07 18:22:22 +0000

    new commit

--- hackerman/ui/betterexec.py
+++ hackerman/ui/betterexec.py
@@ -11,19 +11,19 @@ class FakeIO(object):
 class BetterExec(object):
 	def __init__(self):
 		self.stdout = FakeIO()
-		self.stderr = FakeIO()
+		#self.stderr = FakeIO()
 	def exec(self, code):
 		stdout = sys.stdout
-		stderr = sys.stderr
+		#stderr = sys.stderr
 		sys.stdout = self.stdout
-		sys.stderr = self.stderr
+		#sys.stderr = self.stderr
 
 		exec(code)
 
 		sys.stdout = stdout
-		sys.stderr = stderr
-		res = self.stdout.mem, self.stderr.mem
+		#sys.stderr = stderr
+		res = self.stdout.mem#, self.stderr.mem
 		self.stdout.mem = ""
-		self.stderr.mem = ""
+		#self.stderr.mem = ""
 		
 		return res
